59bc43e63eed932bc6756c6da68fa818e62ff88c
Steven Lewi <steven@stevenlewi.id>
2026-03-18 07:17:26 +0000

    [LeetCode Sync] - Container With Most Water - Runtime - 6 ms (81.34%), Memory - 59.7 MB (73.85%)

---
+++ src/main/kotlin/p0011_container_with_most_water/solution.kt
@@ -0,0 +1,32 @@
+package p0011_container_with_most_water
+
+class Solution {
+
+    // O(n^2)
+    fun maxArea2(height: IntArray): Int {
+        var maxArea = 0
+        for (i in 0 until height.size) {
+            for (j in i+1 until height.size) {
+                val area = min(height[i], height[j]) * (j - i)
+                if (area > maxArea) maxArea = area
+            }
+        }
+
+        return maxArea
+    }
+
+    // O(n)
+    fun maxArea(height: IntArray): Int {
+        var maxArea = 0
+        var leftP = 0
+        var rightP = height.size - 1
+        do {
+            val area = min(height[leftP], height[rightP]) * (rightP - leftP)
+            if (area > maxArea) maxArea = area
+            
+            if (height[leftP] < height[rightP]) leftP++
+            else rightP--
+        } while (rightP != leftP)
+        return maxArea
+    }
+}
