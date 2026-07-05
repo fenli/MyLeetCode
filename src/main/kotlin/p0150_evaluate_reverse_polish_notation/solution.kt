71224de8ac8051ba5aad4d5b36cb1e195b85f6ef
Steven Lewi <steven@stevenlewi.id>
2026-07-05 08:47:17 +0000

    [LeetCode Sync] - Evaluate Reverse Polish Notation - Runtime - 8 ms (91.63%), Memory - 45.2 MB (99.20%)

--- src/main/kotlin/p0150_evaluate_reverse_polish_notation/solution.kt
+++ src/main/kotlin/p0150_evaluate_reverse_polish_notation/solution.kt
@@ -5,27 +5,15 @@ class Solution {
 
     fun evalRPN(tokens: Array<String>): Int {
         for (t in tokens) {
-            when(t) {
-                "+" -> {
-                    val top = stack.pop().toInt()
-                    stack.push(stack.pop().toInt() + top)
+            stack.push(
+                when(t) {
+                    "+" -> stack.pop() + stack.pop()
+                    "-" -> 0 - stack.pop() + stack.pop()
+                    "*" -> stack.pop() * stack.pop()
+                    "/" -> (1.0 / stack.pop() * stack.pop()).toInt()
+                    else -> t.toInt()
                 }
-                "-" -> {
-                    val top = stack.pop().toInt()
-                    stack.push(stack.pop().toInt() - top)
-                }
-                "*" -> {
-                    val top = stack.pop().toInt()
-                    stack.push(stack.pop().toInt() * top)
-                }
-                "/" -> {
-                    val top = stack.pop().toInt()
-                    stack.push(stack.pop().toInt() / top)
-                }
-                else -> {
-                    stack.push(t.toInt())
-                }
-            }
+            )
         }
 
         return stack.pop()
